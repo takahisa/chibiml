8a12115c66e07c1f245a516b0aa955932af591e9
takahisa <takahisa.wt@gmail.com>
2020-06-29 17:28:05 +0000

    Fix Copyright

--- compiler/alpha.ml
+++ compiler/alpha.ml
@@ -1,6 +1,6 @@
 (*
  * Chibiml
- * Copyright (c) 2015-2016 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
+ * Copyright (c) 2015-2020 Takahisa Watanabe <linerlock@outlook.com> All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
